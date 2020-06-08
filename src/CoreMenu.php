e03ad671ea254cf9bfa9b70db2ce12e26aff3313
Paul Water <p.r.water@setbased.nl>
2020-06-08 07:15:05 +0200

    Mark active menu item.

--- src/CoreMenu.php
+++ src/CoreMenu.php
@@ -39,24 +39,59 @@ class CoreMenu extends PlaisioObject implements Menu
 
     if ($row===null)
     {
-      $menu  = $this->nub->DL->abcMenuCoreMenuGetDetails($mnuId);
-      $class = $menu['mnu_class'];
-      /** @var MenuGenerator $generator */
-      $generator = new $class($this);
-      $html      = $generator->menu($mnuId);
-
-      $this->nub->DL->abcMenuCoreCachePut($this->nub->company->cmpId,
-                                          $mnuId,
-                                          $this->nub->babel->lanId,
-                                          $this->nub->session->proId,
-                                          $html);
+      $html = $this->generateMenu($mnuId);
     }
     else
     {
       $html = $row['mnc_html'];
     }
 
+    $this->markActiveMenuItem($mnuId);
+
     return $html;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Generates the HTML code of a menu.
+   *
+   * @param int $mnuId The ID of the menu.
+   *
+   * @return string
+   */
+  private function generateMenu(int $mnuId): string
+  {
+    $menu  = $this->nub->DL->abcMenuCoreMenuGetDetails($mnuId);
+    $class = $menu['mnu_class'];
+    /** @var MenuGenerator $generator */
+    $generator = new $class($this);
+    $html      = $generator->menu($mnuId);
+
+    $this->nub->DL->abcMenuCoreCachePut($this->nub->company->cmpId,
+                                        $mnuId,
+                                        $this->nub->babel->lanId,
+                                        $this->nub->session->proId,
+                                        $html);
+
+    return $html;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Highlights the active menu item.
+   *
+   * @param int $mnuId The ID of the menu.
+   */
+  private function markActiveMenuItem(int $mnuId): void
+  {
+    $pagId = $this->nub->pageInfo['pag_id'];
+    $mniId = $this->nub->DL->abcMenuCoreLinkGetItemByPage($mnuId, $pagId);
+
+    if ($mniId!==null)
+    {
+      $id = 'mni-'.$this->nub->obfuscator::encode($mniId, 'mni');
+      $this->nub->assets->jsAdmClassSpecificFunctionCall(__CLASS__, 'markActiveMenuItem', [$id]);
+    }
   }
 
   //--------------------------------------------------------------------------------------------------------------------
